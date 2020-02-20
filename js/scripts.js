f412094b347c3c098eb15e6d1bb2267d99bebc0a
Andrew Kim <andrewswkim47@gmail.com>
2020-02-19 18:15:06 -0800

    start getting values from checkbox.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//back end logic
+
+
+
+
+
+
+
+
+
+//user interface logic
+
+$(document).ready(function() {
+  $("form#price-show").submit(function(event) {
+    var mushrooms = $("input[type='checkbox']").val();
+    
+
+
+
+
+
+  })
+});
